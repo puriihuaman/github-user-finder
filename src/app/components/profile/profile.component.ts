6f342f2c32a5157d195e421460bd2077ed6e6703
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-09-02 12:57:57 -0500

    fix: add date format

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, DatePipe } from "@angular/common";
 import { Component, Input, type OnInit } from "@angular/core";
 import { IconSvgComponent } from "@components/icon-svg/icon-svg.component";
 import type { User } from "@core/interface/iuser";
@@ -6,7 +6,7 @@ import type { User } from "@core/interface/iuser";
 @Component({
   selector: "app-profile",
   standalone: true,
-  imports: [AsyncPipe, IconSvgComponent],
+  imports: [AsyncPipe, DatePipe, IconSvgComponent],
   templateUrl: "./profile.component.html",
   styleUrl: "./profile.component.scss",
 })
